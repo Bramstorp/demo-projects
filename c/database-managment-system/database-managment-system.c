36b2b397e47c72116f8cf35268296d8859fc6466
Bramstorp <bramstorpmathias@gmail.com>
2024-07-02 20:25:55 +0200

    feat: base login.

--- c/database-managment-system/database-managment-system.c
+++ c/database-managment-system/database-managment-system.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_USERNAME_LENGTH 50
-#define MAX_PASSWORD_LENGTH 50
+#define MAX_LENGTH 50
 
 int main()
 {
-    char username[MAX_USERNAME_LENGTH];
-    char password[MAX_PASSWORD_LENGTH];
+    char username[MAX_LENGTH];
+    char password[MAX_LENGTH];
 
-    printf("Welcome to the Database Management System\n");
+    printf("Welcome to the login screen\n");
 
-    printf("Enter your username: ");
+    printf("Username: ");
     scanf("%s", username);
+    getchar();
 
-    printf("Enter your password: ");
-    scanf("%s", password);
+    printf("Password: ");
+    scanf(password);
+    getchar();
 
-    if (strcmp(username, "admin") == 0 && strcmp(password, "admin") == 0)
+    if (strcmp(username, "admin") == 0 && strcmp(password, "password123") == 0)
     {
-        printf("Login successful\n");
+        printf("Login successful!\n");
     }
     else
     {
-        printf("Login failed\n");
+        printf("Login failed. Incorrect username or password.\n");
     }
 
     return 0;

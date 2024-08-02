5eeb61e3bd8e1e18abb4f7f8dcfbd1916d5d2264
Bramstorp <bramstorpmathias@gmail.com>
2024-08-03 00:31:07 +0200

    fix: buffer overfloq

--- c/database-managment-system/includes/buffer_pool.h
+++ c/database-managment-system/includes/buffer_pool.h
@@ -8,17 +8,17 @@
 
 typedef struct
 {
-    int page_id;
-    char data[PAGE_SIZE];
-    int is_dirty;
-    int pin_count;
-} Page;
+    int page_id;          // the unique identifier of the page
+    char data[PAGE_SIZE]; // the actual data stored in the page
+    int is_dirty;         // 1 if the page has been modified, 0 otherwise
+    int pin_count;        // number of users currently using the page
+} Page;                   // a fixed-size block of data
 
 typedef struct
 {
-    Page pages[BUFFER_POOL_SIZE];
-    int page_table[BUFFER_POOL_SIZE];
-} BufferPool;
+    Page pages[BUFFER_POOL_SIZE];     // the actual pages stored in the buffer pool
+    int page_table[BUFFER_POOL_SIZE]; // maps page_id to index in pages array
+} BufferPool;                         // a collection of pages
 
 void init_buffer_pool();
 
